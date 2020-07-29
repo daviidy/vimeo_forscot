a7f8672856236a55bea38f9aaaaf1fe05b368779
aouman <37551703+aouman@users.noreply.github.com>
2020-07-29 17:27:48 +0000

    login and register, home default page, home admin page

---
+++ resources/views/layouts/menu-default.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="/css/home_default.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+	</head>
+	<body cz-shortcut-listen="true" class="" style="margin:0!important">
+		@yield('content')
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script>
+		$(document).ready(function(){
+		  $(".popup-btn").click(function(){
+		    $(".Card-jmx009-0").toggle(50);
+		  });
+
+			$(".hSKmej").click(function(){
+		    $(".fFYzWN").toggle();
+		  });
+		});
+		</script>
+	</body>
+</html>
