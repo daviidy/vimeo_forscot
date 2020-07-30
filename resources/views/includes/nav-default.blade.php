46e6f154fc15ee15b7810202914d421fbd5a58d9
aouman <37551703+aouman@users.noreply.github.com>
2020-07-30 14:23:21 +0000

    new home default page

---
+++ resources/views/includes/nav-default.blade.php
@@ -0,0 +1,146 @@
+<div width="medium" class="SplitItem-sc-3xe4fi-1 iSdzAX showMenu">
+              <div height="full" class="Container-sc-14tr3x4-0 cLxjui">
+                <div class="ScrollContentRoot-sc-1ukpyz-2 cKSoih">
+                  <div class="TopSectionWrapper-sc-1ukpyz-0 htlbiW">
+                    <div class="Container-sc-14tr3x4-0 dXwQFy">
+                      <div orientation="horizontal" class="Spread-sc-1pjvgl-0 hBfqtO">
+                        <div orientation="horizontal" class="Distribute-oruih0-0 hcuFtA"><span color="#262627" font-weight="medium" font-family="sans" class="Text-wqps8m-0 JjprQ">Mes vidéos</span></div>
+                        <div orientation="horizontal" class="Distribute-oruih0-0 bQSMLn">
+                          <div class="Trigger-sc-132zjtq-2 fdtmxm"><button aria-label="Add workspace" color="#737373" data-qa="add-workspace-button" class="IconButtonRoot-i7dp45-0 efmjko">
+                              <div height="auto" width="auto" color="#737373" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 gEOMsw"><span class="SVGInline"><svg class="SVGInline-svg" width="10" height="10" viewBox="0 0 10 10" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 6c0-1.10457.89543-2 2-2h8c0 1.10457-.89543 2-2 2H0z"></path>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6 0v8c0 1.10457-.89543 2-2 2V2c0-1.104569.89543-2 2-2z"></path>
+                                  </svg></span></div>
+                            </button></div>
+                          <div class="Trigger-sc-132zjtq-2 fdtmxm"><button aria-label="Search" color="#737373" data-qa="search-button" class="IconButtonRoot-i7dp45-0 efmjko">
+                              <div height="auto" width="auto" color="#737373" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 gEOMsw"><span class="SVGInline"><svg class="SVGInline-svg" width="13" height="13" viewBox="0 0 13 13" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                      d="M9.9502 8.77257c.6725-.91404 1.0744-2.03855 1.0744-3.26027C11.0246 2.4675 8.55709 0 5.5123 0 2.4675 0 0 2.4675 0 5.5123c0 3.04479 2.4675 5.5123 5.5123 5.5123 1.21671 0 2.33721-.3989 3.24824-1.06639l1.86816 1.89819c.6545.6545 1.7168.6545 2.3713 0L9.9502 8.77257zM1.60358 5.5123c0-2.15581 1.75391-3.90872 3.90872-3.90872 2.1548 0 3.90872 1.75391 3.90872 3.90872 0 2.1548-1.75392 3.90872-3.90872 3.90872-2.15481 0-3.90872-1.75392-3.90872-3.90872z">
+                                    </path>
+                                  </svg></span></div>
+                            </button></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div tabindex="0" class="ChildrenWrapper-sc-1ukpyz-3 eWfra">
+                    <div class="Spacer-cjod5i-0 bFphE">
+                      <ul class="ListRoot-lexhmg-0 jFYLjf">
+                        <li size="1" class="ListItem-lexhmg-1 fYsvxc">
+                          <div><a data-qa="workspace-item" data-qa-workspace-id="xkV3Cw" href="/accounts/01DX48P3KBMQSCBKYZB47NYYQ6/workspaces/xkV3Cw" class="Link-sc-1bfji0h-0 workspace-list__Link-qj082h-1 eoCqeG">
+                              <div class="workspace-list__WrapperVerticalMenuItem-qj082h-2 JglLJ">
+                                <!--div orientation="horizontal" class="Spread-sc-1pjvgl-0 hBfqtO"><span color="#262627" font-family="sans" class="Text-wqps8m-0 cFlRDP">My workspace</span>
+                                  <div orientation="horizontal" class="Distribute-oruih0-0 beQEXA"><span color="#898989" data-qa="workspace-forms-count-xkV3Cw" font-family="sans"
+                                      class="Text-wqps8m-0 workspace-list__FormsCount-qj082h-0 bvYcyn">3</span></div>
+                                </div-->
+                              </div>
+                            </a></div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="BottomSectionWrapper-sc-1ukpyz-1 JQYyd">
+                    <div class="workspace-list__Wrapper-qj082h-3 knVhB">
+                      <div class="help-center-content__Wrapper-sc-1x892rx-2 fdSYvF">
+                        <div class="Container-sc-14tr3x4-0 egQjir">
+                          <button aria-expanded="false" class="help-center-content__TitleWrapper-sc-1x892rx-0 hSKmej">
+                            <div orientation="horizontal" class="Spread-sc-1pjvgl-0 jhOqJf">
+                              <span font-weight="medium" font-family="sans" class="Text-wqps8m-0 drnBiU">Platform de formation Forscot</span>
+                              <div height="auto" width="auto" color="#AAA" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 cdQlVN">
+                                <span class="SVGInline">
+                                  <svg class="SVGInline-svg" width="12" height="7" viewBox="0 0 12 7" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                      d="M6.00008 2.94976L8.87876 5.82845C9.65981 6.6095 10.9261 6.60949 11.7072 5.82844L6.00008 0.121338L0.292969 5.82844C1.07402 6.60949 2.34035 6.60949 3.1214 5.82844L6.00008 2.94976Z"></path>
+                                  </svg>
+                                </span>
+                              </div>
+                            </div>
+                          </button>
+                          <div class="help-center-content__ContentWrapper-sc-1x892rx-3 fFYzWg">
+                            <div class="Container-sc-14tr3x4-0 gaRaRN">
+                              <ul class="ListRoot-lexhmg-0 jFYLjf">
+                                <li class="ListItem-lexhmg-1 dpyYhN"><a color="#898989" href="https://www.typeform.com/help/" rel="noopener noreferrer" tabindex="-1" target="_blank" class="Link-sc-1bfji0h-0 eUeiwM">
+                                    <div class="help-center-content__ItemWrapper-sc-1x892rx-1 vTYJl">
+                                      <div orientation="horizontal" class="Distribute-oruih0-0 iMvuxr">
+                                        <div height="8px" width="8px" color="#4FB0AE" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 byiYvC"><span class="SVGInline"><svg class="SVGInline-svg" width="8" height="12" viewBox="0 0 8 12"
+                                              xmlns="http://www.w3.org/2000/svg">
+                                              <g fill-rule="evenodd">
+                                                <path
+                                                  d="M0 5h2v.088H0V5zm0-1.25C0 1.494 1.626 0 4 0c2.377 0 4 1.488 4 3.75 0 1.462-.636 2.323-1.885 3.164l-.44.29c-.33.221-.482.355-.54.455C5.068 7.774 4.999 8.25 5 9l-2 .003c-.002-1.083.108-1.835.405-2.347.255-.439.59-.732 1.158-1.113l.435-.287C5.75 4.748 6 4.41 6 3.75 6 2.633 5.309 2 4 2c-1.305 0-2 .638-2 1.75v1.338H0V3.75z"
+                                                  fill-rule="nonzero"></path>
+                                                <path d="M3 10h2v2H3z"></path>
+                                              </g>
+                                            </svg></span></div>
+                                        <div class="Container-sc-14tr3x4-0 ieWpba">Help Center</div>
+                                      </div>
+                                    </div>
+                                  </a></li>
+                                <li class="ListItem-lexhmg-1 dpyYhN"><a color="#898989" href="https://www.typeform.com/help/my-1st-typeform/" rel="noopener noreferrer" tabindex="-1" target="_blank" class="Link-sc-1bfji0h-0 eUeiwM">
+                                    <div class="help-center-content__ItemWrapper-sc-1x892rx-1 vTYJl">
+                                      <div orientation="horizontal" class="Distribute-oruih0-0 iMvuxr">
+                                        <div height="8px" width="8px" color="#e04f78" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 bpFEdq"><span class="SVGInline"><svg class="SVGInline-svg" width="14" height="14" viewBox="0 0 14 14" fill="none"
+                                              xmlns="http://www.w3.org/2000/svg">
+                                              <path
+                                                d="M7.64345.467259L8.98398 4.5934h4.33832c.6554 0 .9275.83827.3977 1.22371l-3.5097 2.55024 1.3405 4.12615c.2026.623-.5105 1.1414-1.0408.7564l-3.50969-2.5506-3.50969 2.5502c-.53026.385-1.24339-.133-1.04083-.7565l1.34053-4.12609-3.510132-2.5498C-.250072 5.4321.022496 4.5934.677883 4.5934H5.01622L6.35674.467259c.20257-.623012 1.08458-.623012 1.28671 0z">
+                                              </path>
+                                            </svg></span></div>
+                                        <div class="Container-sc-14tr3x4-0 ieWpba">Learn the basics</div>
+                                      </div>
+                                    </div>
+                                  </a></li>
+                                <li class="ListItem-lexhmg-1 dpyYhN"><a color="#898989" href="https://www.typeform.com/help/what-is-logic-jump/" rel="noopener noreferrer" tabindex="-1" target="_blank" class="Link-sc-1bfji0h-0 eUeiwM">
+                                    <div class="help-center-content__ItemWrapper-sc-1x892rx-1 vTYJl">
+                                      <div orientation="horizontal" class="Distribute-oruih0-0 iMvuxr">
+                                        <div height="8px" width="8px" color="#ffba49" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 hcFxEI"><span class="SVGInline"><svg class="SVGInline-svg" width="11" height="11" viewBox="0 0 11 11" fill="none"
+                                              xmlns="http://www.w3.org/2000/svg">
+                                              <path
+                                                d="M9.27497 7.55658c-.33409.00018-.66088.09777-.94036.28083-.27947.18306-.4995.44365-.63313.74985H5.35155c-.30164.00002-.5949-.09921-.83456-.28236-.23965-.18316-.4124-.44008-.49157-.73114L2.78861 3.04568c.21632-.17081.38836-.39117.50159-.64246h4.41128c.15786.3617.43544.65801.78608.83912.35063.1811.75294.23595 1.13926.15532.38628-.08063.73308-.29182.98198-.59806.249-.30624.3848-.68885.3848-1.08349 0-.39465-.1358-.777256-.3848-1.083495-.2489-.306239-.5957-.517429-.98198-.5980587C9.2405-.0460735 8.83819.00877499 8.48756.189879c-.35064.181104-.62822.477417-.78608.839111H3.2902C3.17149.754064 2.98297.514944 2.74333.33535 2.5037.155757 2.22128.0419287 1.92408.00515697 1.62688-.0316148 1.32523.00994713 1.04905.12572.772874.241493.531754.427454.349615.665161.167476.902867.0506461 1.18406.0107074 1.48085c-.0399386.29679-.00159613.59886.1112256.87626.112821.2774.296203.52048.531954.70515.23575.18466.515683.30448.812023.34759l1.23337 4.52808c.15836.58211.50384 1.09596.98316 1.46227.47932.36632 1.06584.56476 1.66911.56472h2.34993c.12442.28338.32311.52788.57505.70768.25194.1798.54778.2882.85624.3137.30846.0255.61808-.0328.89613-.1687.2781-.136.5142-.3446.6835-.6037.1693-.25908.2654-.55912.2783-.86837.0128-.30925-.0582-.61622-.2054-.88845-.1473-.27224-.3654-.49963-.6312-.65814-.26588-.15851-.56961-.24225-.87913-.24236z">
+                                              </path>
+                                            </svg></span></div>
+                                        <div class="Container-sc-14tr3x4-0 ieWpba">Branch or skip questions</div>
+                                      </div>
+                                    </div>
+                                  </a></li>
+                                <li class="ListItem-lexhmg-1 dpyYhN"><a color="#898989" href="https://www.typeform.com/help/what-are-hidden-fields/" rel="noopener noreferrer" tabindex="-1" target="_blank" class="Link-sc-1bfji0h-0 eUeiwM">
+                                    <div class="help-center-content__ItemWrapper-sc-1x892rx-1 vTYJl">
+                                      <div orientation="horizontal" class="Distribute-oruih0-0 iMvuxr">
+                                        <div height="8px" width="8px" color="#3C6997" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 dwvYkB"><span class="SVGInline"><svg class="SVGInline-svg" width="16" height="15" viewBox="0 0 16 15" fill="none"
+                                              xmlns="http://www.w3.org/2000/svg">
+                                              <path
+                                                d="M9.4995 7.13445L16 0.633951C15.219 -0.147049 14.0215 -0.216049 13.325 0.480451L11.08 2.72545C10.12 2.36745 9.084 2.16695 8 2.16695C4.494 2.16695 1.4615 4.20395 0 7.16695C0.709 8.60395 1.789 9.81945 3.112 10.693L0.069 13.736C0.85 14.517 2.0475 14.586 2.744 13.8895L8.006 8.62745C8.8295 8.62445 9.496 7.95795 9.4995 7.13445ZM4.5 7.12845C4.5 5.19545 6.067 3.62845 8 3.62845C8.6265 3.62845 9.2105 3.79795 9.7185 4.08645L8.1605 5.64445C8.107 5.63845 8.055 5.62845 8 5.62845C7.1715 5.62845 6.5 6.29995 6.5 7.12845C6.5 7.18345 6.5105 7.23545 6.516 7.28895L4.958 8.84695C4.6695 8.33895 4.5 7.75495 4.5 7.12845Z">
+                                              </path>
+                                              <path
+                                                d="M11.4945 7.23745C11.4375 9.08295 9.9545 10.566 8.109 10.623L6.666 12.0655C7.1015 12.1315 7.5465 12.167 8 12.167C11.506 12.167 14.5385 10.13 16 7.16695C15.526 6.20545 14.884 5.34395 14.1165 4.61545L11.4945 7.23745Z">
+                                              </path>
+                                            </svg></span></div>
+                                        <div class="Container-sc-14tr3x4-0 ieWpba">Pull in info you already know</div>
+                                      </div>
+                                    </div>
+                                  </a></li>
+                                <li class="ListItem-lexhmg-1 dpyYhN"><a color="#898989" href="https://www.typeform.com/help/embed-a-typeform/" rel="noopener noreferrer" tabindex="-1" target="_blank" class="Link-sc-1bfji0h-0 eUeiwM">
+                                    <div class="help-center-content__ItemWrapper-sc-1x892rx-1 vTYJl">
+                                      <div orientation="horizontal" class="Distribute-oruih0-0 iMvuxr">
+                                        <div height="8px" width="8px" color="#a086c4" class="Align-sc-1shbtnq-0 IconRoot-tvakxr-0 ibdGyK"><span class="SVGInline"><svg class="SVGInline-svg" width="14" height="14" viewBox="0 0 14 14"
+                                              xmlns="http://www.w3.org/2000/svg">
+                                              <g fill-rule="evenodd">
+                                                <rect width="10" height="10" rx="1"></rect>
+                                                <path d="M0 7h1v6h12V1H7V0h6a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7zm0 0h1v6h12V1H7V0h6a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7z" fill-rule="nonzero"></path>
+                                              </g>
+                                            </svg></span></div>
+                                        <div class="Container-sc-14tr3x4-0 ieWpba">Embed your typeform</div>
+                                      </div>
+                                    </div>
+                                  </a></li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
--- resources/views/layouts/menu-default.blade.php
+++ resources/views/layouts/menu-default.blade.php
@@ -1,26 +1,163 @@
+<!--
+
+=========================================================
+* Now UI Dashboard - v1.5.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->
 <!DOCTYPE html>
-<html>
-	<head>
-		<title></title>
-	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="/css/home_default.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		
-	</head>
-	<body cz-shortcut-listen="true" class="" style="margin:0!important">
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../asset/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../asset/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="../asset/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../asset/css/now-ui-dashboard.css?v=1.5.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../asset/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="orange">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+          CT
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          <img src="https://forscot.oschoolelearning.com/images/schools/logos/1590163066.png" alt="Logo forscot" width="150">
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="home">
+              <i class="now-ui-icons design_app"></i>
+              <p>Tableau de bord</p>
+            </a>
+          </li>
+          <li>
+            <a href="/myvideo">
+              <i class="now-ui-icons media-1_button-play"></i>
+              <p>Mes vidéos</p>
+            </a>
+          </li>
+          <li>
+            <a href="./map.html">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Maps</p>
+            </a>
+          </li>
+          <li>
+            <a href="./notifications.html">
+              <i class="now-ui-icons ui-1_bell-53"></i>
+              <p>Notifications</p>
+            </a>
+          </li>
+          <li>
+            <a href="./user.html">
+              <i class="now-ui-icons users_single-02"></i>
+              <p>User Profile</p>
+            </a>
+          </li>
+          <li>
+            <a href="./tables.html">
+              <i class="now-ui-icons design_bullet-list-67"></i>
+              <p>Table List</p>
+            </a>
+          </li>
+          <li>
+            <a href="./typography.html">
+              <i class="now-ui-icons text_caps-small"></i>
+              <p>Typography</p>
+            </a>
+          </li>
+          <li class="active-pro">
+            <a href="./upgrade.html">
+              <!--i class="now-ui-icons arrows-1_cloud-download-93"></i-->
+              <p>Platform de formation forscot</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel" id="main-panel">
+      <!-- Navbar -->
 		@yield('content')
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script>
-		$(document).ready(function(){
-		  $(".popup-btn").click(function(){
-		    $(".Card-jmx009-0").toggle(50);
-		  });
+		<footer class="footer">
+	        <div class=" container-fluid ">
+	          <nav>
+	            <ul>
+	              <li>
+	                <a href="https://www.creative-tim.com">
+	                  Creative Tim
+	                </a>
+	              </li>
+	              <li>
+	                <a href="http://presentation.creative-tim.com">
+	                  About Us
+	                </a>
+	              </li>
+	              <li>
+	                <a href="http://blog.creative-tim.com">
+	                  Blog
+	                </a>
+	              </li>
+	            </ul>
+	          </nav>
+	          <div class="copyright" id="copyright">
+	            &copy; <script>
+	              document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+	            </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+	          </div>
+	        </div>
+	      </footer>
+	    </div>
+	  </div>
+	  <!--   Core JS Files   -->
+	  <script src="../asset/js/core/jquery.min.js"></script>
+	  <script src="../asset/js/core/popper.min.js"></script>
+	  <script src="../asset/js/core/bootstrap.min.js"></script>
+	  <script src="../asset/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+	  <!--  Google Maps Plugin    -->
+	  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+	  <!-- Chart JS -->
+	  <script src="../asset/js/plugins/chartjs.min.js"></script>
+	  <!--  Notifications Plugin    -->
+	  <script src="../asset/js/plugins/bootstrap-notify.js"></script>
+	  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+	  <script src="../asset/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+	  <script src="../asset/demo/demo.js"></script>
+	  <script>
+	    $(document).ready(function() {
+	      // Javascript method's body can be found in asset/js/demos.js
+	      demo.initDashboardPageCharts();
 
-			$(".hSKmej").click(function(){
-		    $(".fFYzWN").toggle();
-		  });
-		});
-		</script>
+	    });
+	  </script>
 	</body>
-</html>
+
+	</html>
