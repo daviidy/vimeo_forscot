61a08076fae1845aeea3632bad7f5a4c6e9ea909
aouman <37551703+aouman@users.noreply.github.com>
2020-08-13 08:55:44 +0000

    status comment

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -117,12 +117,13 @@ textarea.form-control{height:inherit!important;}
 @font-face{font-family:'Playfair Display';font-style:normal;font-weight:700;src:url(https://fonts.gstatic.com/s/playfairdisplay/v21/nuFiD-vYSZviVYUb_rj3ij__anPXDTzYgA.woff2) format('woff2');unicode-range:U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;}
       </style>
 
-      @if(session('status'))
-      <div style="margin: 3rem;" class="alert alert-success">
-          {{session('status')}}
-      </div>
-      @endif
-      <div class="panel-header panel-header-sm">
+
+      <div class="panel-header panel-header-sm" style="height:160px;padding-top:25px">
+        @if(session('status'))
+        <div style="margin: 3rem;" class="alert alert-success">
+            {{session('status')}}
+        </div>
+        @endif
       </div>
       <div class="content">
         <div class="row">
